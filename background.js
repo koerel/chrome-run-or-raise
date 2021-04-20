3af9e6c0abcb654a8500117dab9bdd02ec0ba1c2
Karel Eeckelaert <mail@kareleeckelaert.be>
2021-04-20 10:59:36 +0200

    first commit

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.tabs.onCreated.addListener(async (tab) => {
+  const urlString = tab.pendingUrl
+  if (!urlString.startsWith('http://run-or-raise')) {
+    return
+  }
+  await chrome.tabs.remove(tab.id)
+  const [trigger, titleString, url] = urlString.split('#')
+  const title = titleString.replace('_', ' ')
+  const tabs = await chrome.tabs.query({})
+  const found = tabs.filter(t => t.title.includes(title))
+  if (found.length) {
+    await chrome.tabs.highlight({tabs: found[0].index})
+  } else {
+    await chrome.tabs.create({url})
+  }
+})
